3f2452397d97cd51ee0349bc6040b714d55bd39b
saravanan2k03 <112747138+saravanan2k03@users.noreply.github.com>
2023-08-10 11:27:00 +0530

    Update updatetask.js

--- router/updatetask.js
+++ router/updatetask.js
@@ -5,14 +5,14 @@ const dynamodb = require('../config')
 
 route.put('/Todo/:id', (req, res) => {
     const productId = req.params.id;
-    let product = {
+    let Todo = {
         ProductId: { N: productId },
         TaskName: { S: req.body.name },
         Assign: { N: req.body.assign }
     };
     const params = {
         TableName: "Todo",
-        Item: product
+        Item: Todo
     };
     dynamodb.putItem(params, (err) => {
         if (err) {
